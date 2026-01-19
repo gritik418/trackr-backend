9e730179a0e68cffbd0cf3323a02d8ead6cd1840
Ritik <160255700+gritik418@users.noreply.github.com>
2026-01-19 18:37:30 +0530

    feat: prisma setup complete

---
+++ prisma/migrations/20260119130506/migration.sql
@@ -0,0 +1,23 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[username]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `username` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Made the column `name` on table `User` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('USER', 'ADMIN');
+
+-- CreateEnum
+CREATE TYPE "UserStatus" AS ENUM ('ACTIVE', 'INACTIVE', 'BANNED');
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "isVerified" BOOLEAN NOT NULL DEFAULT false,
+ADD COLUMN     "role" "UserRole" NOT NULL DEFAULT 'USER',
+ADD COLUMN     "status" "UserStatus" NOT NULL DEFAULT 'ACTIVE',
+ADD COLUMN     "username" TEXT NOT NULL,
+ALTER COLUMN "name" SET NOT NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
