18c44a8224685c18ab79d69296c8048d7c73abac
Ritik <160255700+gritik418@users.noreply.github.com>
2026-03-02 22:28:17 +0530

    feat: add role based access in org stats route

--- src/dashboard/dashboard.controller.ts
+++ src/dashboard/dashboard.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Req, UseGuards } from '@nestjs/common';
 import { DashboardService } from './dashboard.service';
+import { Request } from 'express';
+import { AuthGuard } from 'src/common/guards/auth/auth.guard';
 
+@UseGuards(AuthGuard)
 @Controller('organizations/:orgId/dashboard')
 export class DashboardController {
   constructor(private readonly dashboardService: DashboardService) {}
 
   @Get('overview')
-  getDashboardStats(@Param('orgId') orgId: string) {
-    return this.dashboardService.getDashboardStats(orgId);
+  getDashboardStats(@Param('orgId') orgId: string, @Req() req: Request) {
+    return this.dashboardService.getDashboardStats(orgId, req);
   }
 }
--- src/dashboard/dashboard.service.ts
+++ src/dashboard/dashboard.service.ts
@@ -1,4 +1,11 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Request } from 'express';
 import { SubscriptionStatus } from 'generated/prisma/enums';
 import { PlanLimits } from 'src/plans/interfaces/plans.interface';
 import { PrismaService } from 'src/prisma/prisma.service';
@@ -7,7 +14,10 @@ import { PrismaService } from 'src/prisma/prisma.service';
 export class DashboardService {
   constructor(private readonly prismaService: PrismaService) {}
 
-  async getDashboardStats(orgId: string) {
+  async getDashboardStats(orgId: string, req: Request) {
+    const userId = req.user?.id;
+    if (!userId) throw new UnauthorizedException('Unauthenticated');
+
     if (!orgId)
       throw new BadRequestException('Please provide organization ID.');
 
@@ -17,7 +27,37 @@ export class DashboardService {
       },
     });
 
-    if (!organization) throw new BadRequestException('Organization not found.');
+    const user = await this.prismaService.organizationMember.findFirst({
+      where: {
+        organizationId: orgId,
+        userId,
+      },
+    });
+
+    if (!user)
+      throw new ForbiddenException(
+        'You are not authorized to access this organization.',
+      );
+
+    if (!organization) throw new NotFoundException('Organization not found.');
+
+    const userRole = user.role;
+
+    if (userRole === 'MEMBER') {
+      const membersCount = await this.prismaService.organizationMember.count({
+        where: {
+          organizationId: orgId,
+        },
+      });
+
+      return {
+        success: true,
+        message: 'Dashboard stats found.',
+        stats: {
+          membersCount,
+        },
+      };
+    }
 
     const [
       membersCount,
@@ -56,6 +96,16 @@ export class DashboardService {
         take: 5,
         orderBy: {
           createdAt: 'desc',
+        },
+        include: {
+          user: {
+            select: {
+              id: true,
+              name: true,
+              email: true,
+              avatarUrl: true,
+            },
+          },
         },
       }),
     ]);
